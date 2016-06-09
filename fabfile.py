1d73556f5ad5f927ddd6d992474c58d4c1b41c43
Christian Sauer <sauerc@gmail.com>
2016-06-09 13:50:31 -0400

    deploy script: create venv with specific python version

--- fabfile.py
+++ fabfile.py
@@ -45,7 +45,7 @@ def deploy():
     with settings(sudo_user='hn_frontpage'):
 
         with cd('/home/hn_frontpage/run/%(stamp)s' % env):
-            sudo('virtualenv venv' % env)
+            sudo('virtualenv venv -p $(pyenv prefix 2.7.11)/bin/python' % env)
 
             with path('./venv/bin', behavior='prepend'):
                 sudo('pip install --quiet --no-cache-dir -r ./src/_requirements/default.txt' % env)
