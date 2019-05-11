92d6833344e0a37ad0087df37b0cea6c8f42e1b2
Christian Sauer <sauerc@gmail.com>
2019-05-11 14:21:59 -0400

    Improve SSE processing

--- firebase/streaming.py
+++ firebase/streaming.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import re
 from abc import ABC
 
 import requests
@@ -13,6 +14,7 @@ class FirebaseStreamingEvents(object):
     process_object = None
     RETURN_EVENTS = frozenset(['keep-alive'])
     RAISE_EVENTS = frozenset(['cancel', 'auth_revoked'])
+    SSE = re.compile(r'event: (.+)\ndata: (.+)')
 
     def __init__(self, process_object):
         self.process_object = process_object
@@ -29,7 +31,7 @@ class FirebaseStreamingEvents(object):
     def _get_data(self):
         while True:
             try:
-                for line in self.request.iter_lines():
+                for line in self.request.iter_lines(chunk_size=1, delimiter=b'\n\n'):
                     yield line
             except requests.exceptions.RequestException as e:
                 _logger.exception("Exception: " + str(e))
@@ -45,14 +47,16 @@ class FirebaseStreamingEvents(object):
             _logger.debug('Data from server: ' + str(line))
             if not line:
                 continue
-            if line[:6] == 'event:':
-                event['event'] = line[7:]
-                continue
-            if line[:5] == 'data:':
-                event['data'] = line[6:]
-                if self._process_event(event) is not None:
-                    break
-                event = {}
+            event = self._process_data(line)
+            if self._process_event(event) is not None:
+                break
+
+    def _process_data(self, raw):
+        values = self.SSE.match(raw)
+        return {
+            'event': values.group(1),
+            'data': values.group(2),
+        }
 
     def _process_event(self, event):
         if not self.process_object.raw_events:
